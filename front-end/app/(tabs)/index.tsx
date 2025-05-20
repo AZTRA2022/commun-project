337530921e7a1123ad72f3d5371509b7c700ae51
AZTRA2022 <konanisaac6594@gmail.com>
2025-05-20 00:32:20 +0000

    premier changement principale , avec initialisation de  NativeWind ,

---
+++ front-end/app/(tabs)/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="text-white" >Hello world</Text>
+    </View>
+  );
+}
